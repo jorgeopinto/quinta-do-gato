e41d65a1c166df07d3ec6032b0da39e1e7541ade
Jorge Oliveira Pinto <jorgeopinto@gmail.com>
2026-04-16 15:51:33 +0100

    update terraform file system

--- modules/azure/network/main.tf
+++ modules/azure/network/main.tf
@@ -86,24 +86,3 @@ resource "azurerm_subnet_network_security_group_association" "qdg-HUB-WE" {
 }
 */
 
-##################################
-#       VNET-PEERINGS            #
-##################################
-
-resource "azurerm_virtual_network_peering" "hub_to_spoke" {
-  name                      = "hub-to-spoke1"
-  resource_group_name       = azurerm_resource_group.qdg_network_dev.name
-  virtual_network_name      = module.vnet-hub.vnet_name
-  remote_virtual_network_id = module.spoke1.vnet_id
-  allow_forwarded_traffic   = true
-  allow_gateway_transit     = true
-}
-
-resource "azurerm_virtual_network_peering" "spoke_to_hub" {
-  name                      = "spoke1-to-hub"
-  resource_group_name       = azurerm_resource_group.qdg_network_dev.name
-  virtual_network_name      = module.spoke1.vnet_name
-  remote_virtual_network_id = module.network.vnet_id
-  allow_forwarded_traffic   = true
-  use_remote_gateways       = true
-}
