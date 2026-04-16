2d78c2847e5b0a68c3b3989cef2f52e2d33c192c
Jorge Oliveira Pinto <jorgeopinto@gmail.com>
2026-04-16 16:32:12 +0100

    update terraform file system

--- modules/azure/network/vnet_peerings/variables.tf
+++ modules/azure/network/vnet_peerings/variables.tf
@@ -2,3 +2,7 @@ variable "vnet_HUB_id" {
     description = "resource Id for HUB Vnet"
     type = string  
 }
+variable "vnet_SPOKE_id" {
+    description = "resource Id for Spoke Vnet"
+    type = string  
+}
