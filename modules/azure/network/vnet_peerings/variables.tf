25145a6f9333377f2745c9e35c2145496e58525c
Jorge Oliveira Pinto <jorgeopinto@gmail.com>
2026-04-21 12:45:26 +0100

    update terraform file system

--- modules/azure/network/vnet_peerings/variables.tf
+++ modules/azure/network/vnet_peerings/variables.tf
@@ -37,7 +37,7 @@ variable "allow_gateway_transit" {
 variable "use_remote_gateways" {
   description = "Usar gateways remotos no Spoke"
   type        = bool
-  default     = false
+  default     = true
 }
 
 variable "allow_forwarded_traffic" {
