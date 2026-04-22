786086c568ae29db04d1e5f95f4fb371fc68df02
Jorge Oliveira Pinto <jorgeopinto@gmail.com>
2026-04-22 10:29:11 +0100

    update terraform file system

--- modules/azure/network/nsg/outputs.tf
+++ modules/azure/network/nsg/outputs.tf
@@ -1,9 +1,9 @@
 output "nsg_ids" {
   description = "Mapa de IDs dos NSGs (nome da subnet => id do NSG)"
-  value       = { for k, v in azurerm_network_security_group.this : k => v.id }
+  value       = { for k, v in azurerm_network_security_group.nsg : k => v.id }
 }
 
 output "nsg_names" {
   description = "Mapa de nomes dos NSGs (nome da subnet => nome do NSG)"
-  value       = { for k, v in azurerm_network_security_group.this : k => v.name }
+  value       = { for k, v in azurerm_network_security_group.nsg : k => v.name }
 }
