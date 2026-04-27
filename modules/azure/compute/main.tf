38495f0f6c207eebc7fbdf30d4a1f1b44a747ac4
Jorge Oliveira Pinto <jorgeopinto@gmail.com>
2026-04-27 17:42:09 +0100

    update terraform file system

--- modules/azure/compute/main.tf
+++ modules/azure/compute/main.tf
@@ -1,6 +1,6 @@
 # Linux Machine Standard_D2s_v3: 
 resource "azurerm_public_ip" "publicIP" {
-for_each = { for vm in var.virtual_machines : vm.name => vm }
+for_each = var.virtual_machines
 
   name                = "PIP-${each.value.name}"
   resource_group_name = var.resource_group_name
