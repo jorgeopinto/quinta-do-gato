3d9ee9fc294413dce7920852820e83ab3664ed23
Jorge Oliveira Pinto <jorgeopinto@gmail.com>
2026-04-29 16:24:33 +0100

    update terraform file system

--- modules/azure/network/udr/main.tf
+++ modules/azure/network/udr/main.tf
@@ -10,16 +10,19 @@ resource "azurerm_route_table" "rt" {
 resource "azurerm_route" "routes" {
   for_each = {
     for subnet_name, subnet in var.subnets :
-    subnet_name => subnet.routes
+    # cria um mapa de rotas por subnet
+    for route in subnet.routes :
+    "${subnet_name}-${route.name}" => {
+      subnet_name = subnet_name
+      route       = route
+    }
     if length(subnet.routes) > 0
   }
 
-  for route in each.value : {
-    name                   = route.name
-    resource_group_name    = var.resource_group_name
-    route_table_name       = azurerm_route_table.rt[each.key].name
-    address_prefix         = route.address_prefix
-    next_hop_type          = route.next_hop_type
-    next_hop_in_ip_address = lookup(route, "next_hop_in_ip_address", null)
-  }
+  name                   = each.value.route.name
+  resource_group_name    = var.resource_group_name
+  route_table_name       = azurerm_route_table.rt[each.value.subnet_name].name
+  address_prefix         = each.value.route.address_prefix
+  next_hop_type          = each.value.route.next_hop_type
+  next_hop_in_ip_address = lookup(each.value.route, "next_hop_in_ip_address", null)
 }
